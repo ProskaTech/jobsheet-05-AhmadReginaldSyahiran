e865e8cc73b87fa6a07444c532289245e2ed8d91
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-09-29 21:06:25 +0700

    Add files via upload

---
+++ src/KataTerakhir1.java
@@ -0,0 +1,23 @@
+/**
+ * Created by 21343039_Ahmad Reginald Syahiran
+ */
+
+import java.util.Scanner;
+
+
+public class KataTerakhir1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter word1 : ");
+        String word1 = in.nextLine();
+
+        System.out.println("Enter word 2 : ");
+        String word2 = in.nextLine();
+
+        System.out.println("Enter word 3 : ");
+        String word3 = in.nextLine();
+
+        System.out.println("\n" + word1 + word2 + word3);
+    }
+}
---
+++ src/KataTerakhir2.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class KataTerakhir2 {
+    public static void main(String[] args) {
+        String word1="", word2="", word3="";
+        word1 = JOptionPane.showInputDialog("Enter word 1   :   ");
+        word2 = JOptionPane.showInputDialog("Enter word 2   :   ");
+        word3 = JOptionPane.showInputDialog("Enter word 3   :   ");
+
+        String msg =  ""+word1+" "+word2+" "+word3+"";
+
+        JOptionPane.showMessageDialog(null, msg);
+
+        System.out.println(""+word1+" "+word2+" "+word3+"");
+
+    }
+    
+}
