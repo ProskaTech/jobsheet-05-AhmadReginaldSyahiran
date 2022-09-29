c2c0e0ea12ddc8078c39499618bcd950c4ca520d
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-09-29 21:15:36 +0700

    Add files via upload

---
+++ src/Scanner1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by 21343039_Ahmad Reginald Syahiran
+ */
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan nama lengkap: ");
+        String nama = in.nextLine();
+
+        System.out.print("Masukkan NIM: ");
+        int nim = in.nextInt();
+
+        System.out.print("Masukkan Nilai: ");
+        float nilai = in.nextFloat();
+
+        System.out.println("\nNama :   " + nama +
+                           "\nNIM  :   " + nim  +
+                           "\nNilai:   " + nilai);
+    }
+}
+
